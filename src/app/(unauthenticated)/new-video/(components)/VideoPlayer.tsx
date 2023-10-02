85197995c1c44d7617540b29aa5e2e8cc7d42a0b
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-10-02 15:45:58 +0100

    fixed build time error

--- src/app/(unauthenticated)/new-video/(components)/VideoPlayer.tsx
+++ src/app/(unauthenticated)/new-video/(components)/VideoPlayer.tsx
@@ -7,29 +7,7 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer = function ({ link }: VideoPlayerProps) {
-  const videoPlayerRef = useRef<HTMLVideoElement>(null)
-  return (
-    <video className="w-full rounded-lg" controls src={link}></video>
-    // <div>
-    //   <video ref={videoPlayerRef} src={link} />
-    //   <div></div>
-    //   <time></time>
-    //   <div>
-    //     <Controls img="play.svg" name="play" />
-    //     <Controls img="volume.svg" name="volume" />
-    //     <Controls img="setting.svg" name="settings" />
-    //   </div>
-    // </div>
-  )
-}
-
-const Controls = function ({ img, name }: { img: string; name: string }) {
-  return (
-    <button>
-      <img src={img} alt={name} />
-      <span>{name}</span>
-    </button>
-  )
+  return <video className="w-full rounded-lg" controls src={link}></video>
 }
 
 export default VideoPlayer
